93952bd6091cb004bfe615f273bc136c19d2a2d0
Michal Noskovic <michal@oneblackbear.com>
2019-09-24 16:28:45 +0100

    change view more link

--- assets/js/NhsFeed/FeedListing.js
+++ assets/js/NhsFeed/FeedListing.js
@@ -38,7 +38,18 @@ export default function FeedListing(props) {
                             </div>
                         ))}
                     </dl>
-                    <span className="nhsuk-button">Read more</span>
+
+                    <div className="nhsuk-action-link">
+                        <span className="nhsuk-action-link__link">
+                            <svg className="nhsuk-icon nhsuk-icon__arrow-right-circle" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                                <path d="M0 0h24v24H0z" fill="none"></path>
+                                <path d="M12 2a10 10 0 0 0-9.95 9h11.64L9.74 7.05a1 1 0 0 1 1.41-1.41l5.66 5.65a1 1 0 0 1 0 1.42l-5.66 5.65a1 1 0 0 1-1.41 0 1 1 0 0 1 0-1.41L13.69 13H2.05A10 10 0 1 0 12 2z"></path>
+                            </svg>
+                            <span className="nhsuk-action-link__text">View Vacancy</span>
+                        </span>
+                    </div>
+
+                    {/*<span className="nhsuk-button">Read more</span>*/}
                     {/*<p class="nhsuk-promo__description">{item.job_description}</p>*/}
                 </div>
             </a>

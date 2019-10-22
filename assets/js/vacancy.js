c0725e4b1fe37fe48b56fb70dd4ce09b9c06499c
Michal Noskovic <michal@oneblackbear.com>
2019-10-22 17:30:35 +0100

    include fetch polypill

--- assets/js/vacancy.js
+++ assets/js/vacancy.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 import NhsFeed from './NhsFeed/NhsFeed';
+import {fetch} from 'whatwg-fetch';
 
 render(
     <NhsFeed feed={window.FEED} />,
